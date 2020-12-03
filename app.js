4c91c46b5beaf2fc8ef9a3f0535096e8a82e8936
TutulDevs <tutulnahid@gmail.com>
2020-12-03 17:32:05 +0600

    initial

---
+++ app.js
@@ -0,0 +1,50 @@
+const $ = s => document.querySelector(s);
+const done = `<h1 class="names">
+Carla & Aaron
+</h1>
+
+<p class="knot">
+Tied the knot on May 15, 2021
+</p>`;
+
+
+function carla () {
+    const theDate = new Date(`May 15 ${new Date().getFullYear() + 1} 14:00:00`);
+    const curTime = new Date();
+    const diff = theDate - curTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+
+
+    $('.secs').textContent = s < 10 ? '0' + s: s;
+    $('.mins').textContent = m < 10 ? '0' + m: m;
+    $('.hours').textContent = h < 10 ? '0' + h: h;
+    $('.days').textContent = d < 10 ? '0' + d: d;
+
+    // When done
+    if (diff < 0) $('.stuffs').innerHTML = done;
+}
+
+// run each seconds
+setInterval(carla, 1000);
+
+
+////////////////////////
+// Modal
+
+$('.info').addEventListener('click', () =>
+    $('.modal').style.display = 'block');
+
+
+$('.close').addEventListener('click', () =>
+    $('.modal').style.display = 'none');
+
+
+window.addEventListener('click', e => {
+    if (e.target == $('.modal'))
+        $('.modal').style.display = 'none';
+});
